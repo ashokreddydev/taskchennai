e9fe3d46e3c8f7fe035427276a8565eea99b6b26
blj8082 <ashokcse505@gmail.com>
2017-05-02 00:55:36 +0530

    Update home.controllers.js

--- app/controllers/home.controllers.js
+++ app/controllers/home.controllers.js
@@ -3,22 +3,22 @@ Alarm.controller('homecontroller',['$scope',  function($scope){
 //localStorage.clear();
     //console.log(localStorage.getItem("alertAlarm"));
    // alert(localStorage.getItem("alertAlarm"))
-     setInterval(function(){ 
+   //  setInterval(function(){ 
     
     
-  var inputDate = new Date("April-26-2017");
-var todaysDate = new Date();
+ // var inputDate = new Date("April-26-2017");
+// var todaysDate = new Date();
 
-if((inputDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)))
-{
+//if((inputDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)))
+//{
 
 
-alert("equal")
-}
+//alert("equal")
+//}
  
     
     
-    }, 100);
+  //  }, 100);
     
     var listOfAlarms=[];
     if(localStorage.getItem("alertAlarm")==null)
